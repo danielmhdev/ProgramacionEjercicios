c4b63bf93a9b867c06c365d14317b17532c7ebff
Daniel <danaelal@gmail.com>
2025-06-12 17:11:56 +0200

    actualizacion ejs

--- src/Mainejercicio7_22_04/MainEjercicio7.java
+++ src/Mainejercicio7_22_04/MainEjercicio7.java
@@ -1,5 +1,7 @@
 package Mainejercicio7_22_04;
 
+import java.util.Scanner;
+
 public class MainEjercicio7 {
     public static void main(String[] args) {
     var numero = 5;
@@ -9,7 +11,17 @@ public class MainEjercicio7 {
 
     int operador1 =9, operador2 =7, resultado = operador1 + operador2;
     System.out.println(resultado);
+/// //////////////////////Ejercicios con arrays /////////////////
 
+        /*
+        4. Buscar un elemento Pide al usuario un número y muestra si está presente en un array, y en qué posición.
+        */
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = {10,25,30,16,58};
+        System.out.println("Introduce el número que quieres buscar: ");
+        int buscado = sc.nextInt();
 
+        boolean encontrado = false;
+        for (int i = 0:   ){
     }
 }
